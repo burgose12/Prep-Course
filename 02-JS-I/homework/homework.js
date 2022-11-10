4f2fa0a937ac0245b399c00e7cc027ba768ecd5e
burgose <burgosemiro@gmail.com>
2022-11-10 09:29:36 -0500

    two problems missing

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -209,33 +209,37 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
- return ('Mi nombre es ') + nombre (' ') + apellido ;
+ return ("Mi nombre es " + nombre + ' ' + apellido) ;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return ('hola ' + nombre, '!') ;
+  return "hola",nombre,"!" ;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = alto * ancho ;
+  return area ;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perimetro = lado * 4 ;
+  return perimetro ;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+var area = (base * altura)/2
+return area ;
 
 }
 
@@ -244,7 +248,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var dolar = euro * 1.20 ;
+  return dolar ;  
 }
 
 
@@ -254,7 +259,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.lentgh > 1){
+    return ('Dato incorrecto') ;
+  }else if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+    return ('Es vocal');
+  } else {
+    return ('Dato incorrecto') ;
+  }
 }
 
 
